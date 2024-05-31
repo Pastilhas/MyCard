0d5f3354465d7afbfac77fdd83d0511c8c4b2b79
João Campos <joao.campos1221@gmail.com>
2024-05-31 11:26:55 +0100

    remove unused

--- app/src/main/java/com/example/mycard/MainActivity.kt
+++ app/src/main/java/com/example/mycard/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -13,12 +14,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,15 +28,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mycard.ui.theme.MyCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,40 +43,79 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyCardTheme {
-                CardApp()
+                Scaffold {
+                    Surface(
+                        modifier = Modifier
+                            .padding(it)
+                            .fillMaxSize()
+                    ) {
+                        CardApp()
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun Card(modifier: Modifier = Modifier) {
+fun Card(
+    type: Int, updateType: (Int) -> Unit, modifier: Modifier = Modifier
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween,
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxSize(),
     ) {
-        Spacer(
-            modifier.weight(1f),
-        )
+        Spacer(Modifier)
 
-        Row(
-            modifier.weight(1f),
-        ) {
-            Picture()
+        Row {
+            when (type) {
+                0 -> Picture()
+                1 -> CodeImage(id = R.drawable.code_whatsapp)
+                2 -> CodeImage(id = R.drawable.code_github)
+                else -> CodeImage(id = R.drawable.code_linkedin)
+            }
         }
 
         Row(
-            modifier.weight(1f),
+            modifier = Modifier
+                .padding(bottom = 48.dp)
+                .fillMaxWidth(),
             verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
-            Column(
-                modifier = modifier.padding(bottom = 48.dp)
-            ) {
-                Contact(contact = stringResource(R.string.mobile), id = R.drawable.telephone)
-                Contact(contact = stringResource(R.string.git_user), id = R.drawable.github)
-                Contact(contact = stringResource(R.string.in_user), id = R.drawable.linkedin)
-            }
+            ContactButton(
+                active = (type == 1),
+                icon = R.drawable.whatsapp,
+                onClick = {
+                    when (type) {
+                        1 -> updateType(0)
+                        else -> updateType(1)
+                    }
+                },
+            )
+
+            ContactButton(
+                active = (type == 2),
+                icon = R.drawable.github,
+                onClick = {
+                    when (type) {
+                        2 -> updateType(0)
+                        else -> updateType(2)
+                    }
+                },
+            )
+
+            ContactButton(
+                active = (type == 3),
+                icon = R.drawable.linkedin,
+                onClick = {
+                    when (type) {
+                        3 -> updateType(0)
+                        else -> updateType(3)
+                    }
+                },
+            )
         }
     }
 }
@@ -94,45 +131,77 @@ fun Picture(modifier: Modifier = Modifier) {
             contentDescription = "avatar",
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .size(200.dp)
-                .clip(CircleShape)
-                .border(1.dp, Color.DarkGray, CircleShape)
-                .aspectRatio(1f)
+                .border(
+                    width = 4.dp,
+                    color = MaterialTheme.colorScheme.tertiaryContainer,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .fillMaxWidth(0.7f)
+                .clip(RoundedCornerShape(16.dp))
         )
 
         Text(
             text = stringResource(R.string.name),
             textAlign = TextAlign.Center,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.displayMedium,
             modifier = modifier.padding(top = 12.dp),
         )
 
         Text(
             text = stringResource(R.string.title),
             textAlign = TextAlign.Center,
-            fontSize = 16.sp,
+            style = MaterialTheme.typography.titleLarge,
             modifier = modifier.padding(top = 6.dp),
         )
     }
 }
 
 @Composable
-fun Contact(contact: String, id: Int, modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically, modifier = modifier.padding(8.dp)
+fun CodeImage(@DrawableRes id: Int, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Surface(
+            color = MaterialTheme.colorScheme.inverseSurface,
+            shape = RoundedCornerShape(16.dp),
+            modifier = modifier,
+        ) {
+            Image(
+                painter = painterResource(id),
+                contentDescription = "avatar",
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.inverseOnSurface),
+                modifier = Modifier
+                    .fillMaxWidth(0.75f)
+                    .aspectRatio(1f)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ContactButton(
+    active: Boolean, @DrawableRes icon: Int, onClick: () -> Unit, modifier: Modifier = Modifier
+) {
+    val sur = when (active) {
+        true -> MaterialTheme.colorScheme.onSecondaryContainer
+        else -> MaterialTheme.colorScheme.secondaryContainer
+    }
+
+    val img = when (active) {
+        true -> MaterialTheme.colorScheme.secondaryContainer
+        else -> MaterialTheme.colorScheme.onSecondaryContainer
+    }
+
+    Surface(
+        color = sur, shape = RoundedCornerShape(16.dp), onClick = onClick, modifier = modifier
     ) {
         Image(
-            painter = painterResource(id),
-            contentDescription = "contact",
-            modifier = modifier.size(24.dp),
-        )
-
-        Text(
-            text = contact,
-            textAlign = TextAlign.Start,
-            fontSize = 24.sp,
-            modifier = modifier.padding(start = 12.dp),
+            colorFilter = ColorFilter.tint(img),
+            painter = painterResource(id = icon),
+            contentDescription = "mobile",
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
@@ -140,45 +209,9 @@ fun Contact(contact: String, id: Int, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun CardApp() {
-    Card()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(
-        Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-    )
-}
-
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableIntStateOf(1) }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Image(
-            painter = painterResource(
-                id = when (result) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> R.drawable.dice_6
-                }
-            ),
-            contentDescription = result.toString()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { result = (1..6).random() }) {
-            Text(text = stringResource(R.string.roll))
-        }
+    var type by remember {
+        mutableIntStateOf(0)
     }
+
+    Card(type, { type = it })
 }
